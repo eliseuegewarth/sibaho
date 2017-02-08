5e0f3c953bd716666086d9820a9f2f7fc1ee2ce9
Eliseu Egewarth <eliseuegewarth@gmail.com>
2017-02-08 21:24:50 -0200

    Adding RamalModel class to Admin panel
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- empregado/admin.py
+++ empregado/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Supervisor, Estagiario, TurnoDeTrabalho
+from .models import *
 
 admin.site.register(Supervisor)
 admin.site.register(Estagiario)
 admin.site.register(TurnoDeTrabalho)
+admin.site.register(RamalModel)
