eb9f5f8b0d189e8e763e8c0ca850e66d794f8512
Eliseu Egewarth <eliseuegewarth@gmail.com>
2017-02-09 01:51:49 -0200

    Adding missig template elements to extends base.html template
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- sibaho/settings.py
+++ sibaho/settings.py
@@ -61,20 +61,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sibaho.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -90,13 +82,3 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.10/topics/i18n/
-
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
